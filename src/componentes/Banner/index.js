0d1c268af38ca2b919b042e2f865c32621f5ce9c
thiago-lemes <t.lemes@nextage.com.br>
2024-04-15 19:04:48 -0300

    frist commit

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Banner.css';
+
+function Banner(){
+    return(
+    <header className='banner'>
+        <img src="/imagens/banner.png" alt="bane de cabeçario" />
+    </header>
+    )
+}
+
+export default Banner
---
+++ src/componentes/Formulario/index.js
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+import Botao from '../Botao'
+import TextField from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
+import './Formulario.css'
+
+
+const Formulario = () => {
+    const times = [
+        'Programação',
+        'Front-end',
+        'Data Science',
+        'Devops',
+        'Ux e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
+    const [nome, setNome] = useState('')
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [time, setTime] = useState('')
+
+    const aoSalvar = (event) => {
+        event.preventDefault()
+        console.log('Form foi submetido')
+    }
+    return (
+        <section className='formulario'>
+            <form onSubmit={aoSalvar}>
+                <h2>Preencha os dados para criar o card do colaborador</h2>
+                <TextField
+                    obrigatorio={true}
+                    label="Nome"
+                    placeholder="Digite seu nome"
+                    valor={nome}
+                    aoAlterado={valor => setNome(valor)}
+                />
+                <TextField
+                    obrigatorio={true}
+                    label="Cargo"
+                    placeholder="Digite seu cargo"
+                    valor={cargo}
+                    aoAlterado={valor => setCargo(valor)}
+                />
+                <TextField
+                    label="Imagem"
+                    placeholder="Digite o endereço da imagem"
+                    valor={imagem}
+                    aoAlterado={valor => setImagem(valor)}
+                />
+                <ListaSuspensa
+                    obrigatorio={true}
+                    label="Time"
+                    itens={times}
+                    valor={time}
+                    aoAlterado={valor => setTime(valor)}
+                />
+                <Botao>
+                    Criar Card
+                </Botao>
+            </form>
+        </section>
+    )
+}
+export default Formulario
